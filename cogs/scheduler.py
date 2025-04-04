e898ca9b24c8b4a4f91037219311c95363830ee8
Katok <Katok@github.com>
2025-04-04 11:43:41 -0400

    Updated scheduler.py with space scheduling and embed formatting

--- cogs/scheduler.py
+++ cogs/scheduler.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+SPACE_ID = int(os.getenv("SPACE_ID"))
 
 class Scheduler(commands.Cog):
     def __init__(self, bot):
@@ -56,6 +57,47 @@ class Scheduler(commands.Cog):
                                         discord.Color.gold(), "☀️")
         ))
 
+        # SPACE Reminder
+        schedule.every().tuesday.at("06:00").do(lambda: asyncio.create_task(
+        self.send_space_message(
+            "***Incoming Transmission from Squadron HQ, Crimson Hollow***",
+            "Happy Chewsday Pilots!",
+            "As a reminder, space PvP starts at 10pm GMT (<t:1743957600:R> / <t:1743957600:F>). Prepare to group up and head to Deep Space!",
+            discord.Color.dark_red(),
+            "<:TieDefender:682583044783341570>",
+            "images/abyssal_squadron_banner.jpg"
+        )))
+
+        schedule.every().tuesday.at("16:00").do(lambda: asyncio.create_task(
+        self.send_space_message(
+            "***Incoming Transmission from Squadron HQ, Crimson Hollow***",
+            "Chewsday Night Approaches!",
+            "Chewsday night Space PvP is about to start! Let's get ready to rumble and meet in Deep Space <t:1743957600:R>.",
+            discord.Color.dark_red(),
+            "<:TieDefender:682583044783341570>",
+            "images/abyssal_squadron_banner.jpg"
+        )))
+
+        schedule.every().friday.at("10:00").do(lambda: asyncio.create_task(
+        self.send_space_message(
+            "***Incoming Transmission from Squadron HQ, Crimson Hollow***",
+            "Friday Night Fights!",
+            "Good morning Pilots! Friday Night Fights is on. Calling all US Pilots to Deep Space <t:1746838800:R>. Group up and head to Deep Space!",
+            discord.Color.dark_red(),
+            "<:TieDefender:682583044783341570>",
+            "images/abyssal_squadron_banner.jpg"
+        )))
+
+        schedule.every().friday.at("19:00").do(lambda: asyncio.create_task(
+        self.send_space_message(
+            "***Incoming Transmission from Squadron HQ, Crimson Hollow***",
+            "Weapons Hot!",
+            "Time to stock up on missiles and chaff pilots as Friday Night Fights is kicking off <t:1746838800:R>. Go get 'em!",
+            discord.Color.dark_red(),
+            "<:TieDefender:682583044783341570>",
+            "images/abyssal_squadron_banner.jpg"
+        )))
+
 
     async def scheduler(self):
         await self.bot.wait_until_ready()
@@ -83,6 +125,19 @@ class Scheduler(commands.Cog):
                 print(f"⚠️ Error sending image: {e}")
         else:
             print(f"⚠️ Channel with ID {CHANNEL_ID} not found!")
+
+    async def send_space_message(self, standard_message, title, message, color, emoji, image_path):
+        channel = self.bot.get_channel(SPACE_ID)
+        if channel:
+            filename = image_path.split('/')[-1]
+            embed = discord.Embed(title=title, description=message, color=color)
+            embed.set_footer(text="📅 Scheduled Notification")
+            embed.set_image(url=f"attachment://{filename}")
+            file = discord.File(image_path, filename=filename)
+            sent_message = await channel.send(content=standard_message, embed=embed, file=file)
+            await sent_message.add_reaction(emoji)
+        else:
+            print(f"⚠️ Channel with ID {SPACE_ID} not found!")
 
 async def setup(bot):
     await bot.add_cog(Scheduler(bot))
